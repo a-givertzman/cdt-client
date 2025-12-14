00dd7b387df2e8e73b351dd278a20490145602dd
povergo <korolev.petr@2003gmail.com>
2025-12-14 17:58:05 +0300

    PagesSwitch | Fixed initial validation

--- lib/presentation/core/widgets/pages_switch/page_config.dart
+++ lib/presentation/core/widgets/pages_switch/page_config.dart
@@ -13,12 +13,4 @@ class PageConfig {
     required this.name,
     required this.builder,
   });
-  //
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is PageConfig && runtimeType == other.runtimeType && name == other.name;
-  //
-  @override
-  int get hashCode => name.hashCode;
 }
--- lib/presentation/initial_page/widgets/second_page.dart
+++ lib/presentation/initial_page/widgets/second_page.dart
@@ -35,6 +35,7 @@ class _SecondPageState extends State<SecondPage> {
       child: Form(
         key: _formKey,
         onChanged: _updateFormValidity,
+        autovalidateMode: AutovalidateMode.always,
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           spacing: uiPadding,
